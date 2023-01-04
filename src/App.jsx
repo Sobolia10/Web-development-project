6af4d2194c98aada2b17f7745757db78eeed9812
sobolia <sobolia@inbox.ru>
2023-01-04 19:24:24 +0300

    add initial data

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,43 @@ import {Landing} from "./Components/Landing";
 import {ProtectedRoute} from "./Components/ProtectedRoute";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+const useMakeQuery = (initialUrl, initialData) => {
+    const [data, setData] = useState(initialData);
+    const [url, setUrl] = useState(initialUrl);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            setIsLoaded(true)
+            const response = await fetch(url);
+            await wait(1000)
+            const data = await response.json();
+            setData(data);
+
+        } catch (error) {
+            //Ignore
+        }
+        setIsLoading(false);
+    };
+
+    if (!isLoaded)
+        fetchData();
+
+    return [{data, isLoading, isLoaded}, setUrl];
+}
+
 function App() {
+    const [{ isLoaded}, doFetch] = useMakeQuery(
+        'https://jsonplaceholder.typicode.com/users', []);
     const [user, setUser] = useState(null);
+
+    if (!isLoaded)
+        doFetch('https://jsonplaceholder.typicode.com/users');
 
     return (
         <BrowserRouter>
